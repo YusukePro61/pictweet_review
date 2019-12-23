f82256c514b4a9eed9493a802e0dae80c1fce5c2
YusukePro61 <learning630@gmail.com>
2019-12-23 23:43:09 +0900

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tweets' => 'tweets#index'
+  get 'tweets/new' => 'tweets#new'
+  get 'users/:id' => 'users#show'
+  post  'tweets'      =>  'tweets#create'
+  delete 'tweets/:id' => 'tweets#destroy'
+  get 'tweets/:id/edit' => 'tweets#edit'
+  patch 'tweets/:id' => 'tweets#update'
+  root 'tweets#index'
 end
